4e2b033d9c85d9fb3c84e2e34127a41a064a0137
gumi25-art <160398203+gumi25-art@users.noreply.github.com>
2024-06-27 10:58:55 +0800

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+function handleclick() {
+setTimeout(() => {
+    text.textContent = 'ボタンをクリックしました';
+ }, 2000);
+    }
+    
+ btn.addEventListener('click', handleclick);
+    
